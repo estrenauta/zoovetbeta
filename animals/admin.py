5b4cf8d4ecf373fa8ddbde6c1a9336c7678bb572
estrenauta <flojisima@gmail.com>
2018-11-30 22:00:38 -0600

    Borrado de base de datos y creacion de nuevo usuario y superusuario

--- animals/admin.py
+++ animals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserVet
 
 # Register your models here.
+class UserVetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cedula_profesional', 'clinic_name')
+
+admin.site.register(UserVet) #esto importa
--- animals/models.py
+++ animals/models.py
@@ -1,62 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from django.core.validations import 
 
 # Create your models here.
-
-class Animal(models.Model): 
-    name = models.CharField(max_length=200, null= False)
-    chip =  models.IntegerField
-    adoption_day = models.DateField(null= False)
-    species = models.CharField(max_length=45, null= False)
-    breed = models.CharField(max_length=45)
-    color = models.CharField(max_length=45)
-    sex = models.CharField(max_length=45)
-    castrating = models.DateField
-    pedigree = models.CharField(max_length=45)
-    tattoo = models.CharField(max_length=45, unique= True)
-    created_at = models.DateField(auto_now=True)
-    modifed_at = models.DateTimeField(auto_now=True)
-
-class Clinical_Record(models.Model):
-    examinations = models.DateField
-    diagnostic = models.CharField(max_length=45)
-    treatment = models.CharField(max_length=45)
-    created_at = models.DateField(auto_now=True)
-    modifed_at = models.DateField(auto_now=True)
-
-class Inmunization(models.Model):
-    batch = models.IntegerField
-    application_date = models.DateField
-    reapplication_date = models.DateField
-    due_date = models.DateField
-
-class Vaccines(models.Model):
-    name = models.CharField(max_length=45, null= False)
-    description = models.TextField(null = False)
-
-class Wormed(models.Model):
-    application_date = models.DateField(null = False)
-    reapplication_date = models.DateField(null = False)
-
-class Worm(models.Model):
-    name = models.CharField(max_length=45, null= False)
-    description = models.TextField(null = False)
-
-class User(models.Model):
-    username = models.CharField(max_length=16, null = False)
-    email = models.EmailField(null = False)
-    password = models.CharField(max_length=45)
+class UserProfile(models.Model):
     create_at = models.DateField(auto_now= True)
     street = models.CharField(max_length=45)
     city = models.CharField(max_length=45)
     colony = models.CharField(max_length=45)
     state = models.CharField(max_length=45)
     country = models.CharField(max_length=45)
-    zip_code = models.IntegerField
+    zip_code = models.IntegerField()
     phone = models.CharField(max_length=45)
+    user = models.OneToOneField(User, on_delete= models.CASCADE, primary_key= True)
 
-class Uservet(models.Model):
-    licensed = models.IntegerField
+class Vaccine(models.Model):
+    name = models.CharField(max_length=45, null= False)
+    description = models.TextField(null = False)
 
+class Antiparasitic(models.Model):
+    name = models.CharField(max_length=45, null= False)
+    description = models.TextField(null = False)
 
+class Animal(models.Model): 
+    name = models.CharField(max_length=200, null= False)
+    chip =  models.IntegerField(null=True)
+    adoption_day = models.DateField(null= False)
+    species = models.CharField(max_length=45, null= False)
+    breed = models.CharField(max_length=45)
+    color = models.CharField(max_length=45)
+    sex = models.CharField(max_length=45)
+    castrating = models.BooleanField(null= False)
+    pedigree = models.CharField(max_length=45)
+    tattoo = models.CharField(max_length=45, null= True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class ClinicalRecord(models.Model):
+    examination_date = models.DateField()
+    diagnostic = models.TextField()
+    treatment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    vaccines = models.ManyToManyField(Vaccine, through='Inmunization')
+    antiparasitic = models.ManyToManyField(Antiparasitic, through='Deworming')
+
+class Inmunization(models.Model):
+    clinical_record = models.ForeignKey(ClinicalRecord, on_delete=models.CASCADE)
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
+    batch = models.IntegerField()
+    application_date = models.DateField()
+    reapplication_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Deworming(models.Model):
+    clinical_record = models.ForeignKey(ClinicalRecord, on_delete=models.CASCADE)
+    antiparasitic = models.ForeignKey(Antiparasitic, on_delete=models.CASCADE)
+    application_date = models.DateField(null = False)
+    reapplication_date = models.DateField(null = False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class UserVet(models.Model):
+    cedula_profesional = models.IntegerField()
+    clinic_name = models.CharField(max_length = 45)
+    user = models.OneToOneField(User, on_delete= models.CASCADE, primary_key= True)
+
+    def __str__(self):
+        return 'UserVet: {}'.format(self.user.username)
+    
+    #animal = models.ManyToManyField(Animal, through='Animal', related_name='animal')
     #members = models.ManyToManyField (Person, through= 'Memebership')
 
